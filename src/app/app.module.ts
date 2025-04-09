80ed5b4c2d47d239e6f36af3d3dcaf6f7d58979f
Fagner Lisboa <fnlisboa2016@gmail.com>
2025-04-09 11:12:56 -0300

    Creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { ContentComponent } from './pages/content/content.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    MenuTitleComponent,
+    SmallCardComponent,
+    ContentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
